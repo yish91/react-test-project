00210375495946b55d4f038d0007ee58e09400af
Yi Sheng <yish91@hotmail.com>
2018-07-28 04:33:46 +0800

    setting up application

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const ce = React.createElement;
+const App = () => (
+  <div className='app'>
+    <div className='landing'>
+      <h1>Video</h1>
+      <input type='text' placeholder='Search'/>
+      <a>or Browse All</a>
+    </div>
+  </div>
+);
 
-const myTitle = function myTitle(props) {
-  return ce('div', null, ce('h1', null, props.title));
-};
-
-const MyFirstComponent = function myFirstComponent() {
-  return ce(
-    'div',
-    null,
-    ce(myTitle, { title: 'lala' }),
-    ce(myTitle, { title: 'lala2' }),
-    ce(myTitle, { title: 'lala3' }),
-    ce(myTitle, { title: 'test' })
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App/>, document.getElementById('app'));
